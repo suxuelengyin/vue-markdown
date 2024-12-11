4e4f223d0c0cf99f9a3a7202a102bbf9c7fe8970
suxue-mac <suxuewb@gmail.com>
2024-12-11 16:09:02 +0800

    init

---
+++ App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>1111</div>
+</template>
+<script>
+export default {
+  name: 'App',
+  data() {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+    }
+  },
+}
+</script>
---
+++ lib/index.vue
@@ -0,0 +1,11 @@
+<template></template>
+<script>
+export default {
+  name: 'VueMarkdown',
+  data() {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+    }
+  },
+}
+</script>
